6c76e479249455c2e6fc7fb82f9008c9435671a7
desarrolladorpalomino <mesadeayudapalomino@gmail.com>
2020-11-24 16:06:21 -0500

    inicio de dashboard

--- src/views/views.js
+++ src/views/views.js
@@ -4,14 +4,15 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname,'index.html'))
-  //res.redirect("login");
+  res.sendFile(path.join(__dirname, 'index.html'))
 });
 
-/*router.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});*/
-
+router.get("/seguimiento", (req, res) => {
+  res.sendFile(path.join(__dirname, '/templates/seguimiento.html'))
+});
+router.get("/pacientes", (req, res) => {
+  res.sendFile(path.join(__dirname, '/templates/pacientes.html'))
+});
 
 //Exports
 module.exports = router;
